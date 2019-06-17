ec2580d7cc557cb78061fa54965c87d3c9c8b7a6
Christopher Cudiamat <ccudiamat93@gmail.com>
2019-06-16 22:21:27 -0700

    updated js and index

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,23 +1,71 @@
-var rbSingers = ["usher","musiq_soulchild","trey_songz","chris_brown","ella_mai","her"];
+var rbSingers = ["usher","musiq soulchild","daniel caesar","chris brown","ella mai","her"];
 
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 
 var guessedLetter = [];
-var guessWord = [];
+var currentWord = [];
+
+var directionsText = document.getElementById("directions-text");
+
+var randomSing
 
 // START
 
 document.onkeypress = function (event) {
-
+    directionsText.textContent = "";
+    letterCheck (event.key.toUpperCase())
 }
 
 // FUNCTIONS
 
-function Game () {
+function gameStart () {
 
-    randomSing = rbSingers[Math.floor(Math.random() *rbSingers.length)];
+    randomSing = rbSingers[Math.floor(Math.random() *rbSingers.length)].toUpperCase();
+
+    lettersOfWord = randomWord.split("");
+
+    //store length of word in blanks, for later use
+    blanks = lettersOfWord.length;
+
+    //creating a loop to generate "_" for each letter in array stored in blanks
+    for (var i = 0; i < blanks; i++) {
+        blanksAndCorrect.push("_");
+    }
+
+    //showing the "_" within HTML
+    document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join("  ");
+}
+
+// LETTER CHECK
+
+function letterCheck (letter) {
+
+    var guessTrue  = false;
+
+// TRUE STATEMENT
+
+    for (var i = 0; i < blanks; i++) {
+        if (randomSing[i] == letter) {
+            guessTrue = true;
+        }
+    }
+
+// FALSE STATEMENT
+
+    if (guessTrue) {
+        for (var i = 0; i < blanks; i++) {
+            if (randomSing[i] == letter) {
+                blanksAndCorrect[i] = letter;
+            }
+        }
+    }
+
+    else {
+        guessedLetter.push(letter);
+        guessesLeft--;
+    }
 
 }
 
@@ -27,11 +75,14 @@ function reset() {
     guessesLeft= 10;
     guessedLetter = [];
     blanksAndCorrect = [];
-    Game();
+    gameStart();
 }
 
-// DISPLAY
+// DISPLAYED NUMBERS
 
-document.getElementById("wins").innerHTML = "" + wins;
-document.getElementById("losses").innerHTML = "" + losses;
-document.getElementById("guessedLetter").innerHTML = "" + wins;
+document.getElementById("wins").innerText = "" + wins;
+document.getElementById("losses").innerText = "" + losses;
+document.getElementById("guessedLetter").innerText = "" + guessedLetter;
+document.getElementById("guessesLeft").innerText = "" + guessesLeft;
+
+
